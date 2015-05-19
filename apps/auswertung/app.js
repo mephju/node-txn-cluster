95eec96e5fea80793dd3f43dc023728456806abf
Kolja <mephju@gmail.com>
2015-05-19 16:18:25 +0200

    save

--- apps/auswertung/app.js
+++ apps/auswertung/app.js
@@ -1,7 +1,19 @@
 require('../init')
 var Nedb	= require('nedb')
 
-var dataset = app.datasets.movielensCustom
+var dataset = null 
+
+log(process.env.DATASET)
+if(process.env.DATASET === 'movielens') {
+	dataset = app.datasets.movielens
+}
+else if(process.env.DATASET === 'movielensCustom') {
+	dataset = app.datasets.movielensCustom
+}
+else {
+	dataset = app.datsets.movielensCustom
+}
+
 var db = new Nedb({ 
 	filename: dataset.resultPath + 'evaluation-' + dataset.name + '.db' ,
 	autoload: true
@@ -9,20 +21,38 @@ var db = new Nedb({
 
 db
 .find({})
-.sort({ precision: 1 })
+.sort({ 'config.DISTANCE_MEASURE':1, 'config.MARKOV_ORDER':1, 'config.ITEM_CHOICE_STRATEGY':1,'config.CROSS_VALIDATION_RUN':1 })
 .exec(function (err, docs) {
-  docs.forEach(function(doc) {
-	console.log(
+ //  docs.forEach(function(doc) {
+	// console.log(
+	// 	doc.config.DISTANCE_MEASURE, 
+	// 	doc.config.MARKOV_ORDER, 
+	// 	doc.config.ITEM_CHOICE_STRATEGY, 
+	// 	doc.config.CROSS_VALIDATION_RUN,
+	// 	doc.precision
+	// );  	
+	// //console.log(doc.numTxns, doc.numTxnsTraining, doc.numTxnsValidation)
+ //  });
+
+  for(var i=0; i<docs.length; i=i+3) {
+  	
+  	var doc = docs[i]
+  	var precision1 = docs[i].precision
+  	var precision2 = docs[i+1].precision
+  	var precision3 = docs[i+2].precision
+  	
+  	console.log(
 		doc.config.DISTANCE_MEASURE, 
-		doc.config.CROSS_VALIDATION_RUN, 
 		doc.config.MARKOV_ORDER, 
 		doc.config.ITEM_CHOICE_STRATEGY, 
-		doc.precision,
-		new Date(doc.createdAt)
-	);  	
-	//console.log(doc.numTxns, doc.numTxnsTraining, doc.numTxnsValidation)
-  });
-  //console.log(docs[0])
+		doc.config.CROSS_VALIDATION_RUN,
+		precision1,
+		precision2,
+		precision3,
+		(precision1+precision2+precision3)/3.0
+	);	
+  }
+
   console.log(docs.length)
 });
 
