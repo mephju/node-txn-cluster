096fef251cdc3046cf82883ef23e23396d854de5
Kolja <mephju@gmail.com>
2015-07-24 17:19:04 +0200

    s

--- apps/transition-builder/runner.js
+++ apps/transition-builder/runner.js
@@ -33,7 +33,7 @@ var buildTransitions = function(dataset, done) {
 		function(txnRows, next) {
 			log.red('got txns', txnRows.length)
 			var txnRows = txnRows.filter(function(row) {
-				return row['item_ids'].length > 2
+				return row['item_ids'].length > 10
 			})
 			log.red('got txns after', txnRows.length)
 			bag.txnRows = txnRows
