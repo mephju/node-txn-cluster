ff90031bf5764b5d1f8c2de10453d251c1527872
Kolja <mephju@gmail.com>
2015-07-24 17:29:30 +0200

    s

--- apps/transition-builder/runner.js
+++ apps/transition-builder/runner.js
@@ -33,12 +33,12 @@ var buildTransitions = function(dataset, done) {
 		function(txnRows, next) {
 			log.red('got txns', txnRows.length)
 			var txnRows = txnRows.filter(function(row) {
-				return row['item_ids'].length > 4 && row['item_ids'].length < 50 
+				return row['item_ids'].length > 10 && row['item_ids'].length < 40 
 			})
 			
 			log.red('got txns after', txnRows.length)
 			bag.txnRows = txnRows
-		
+			return
 			clustering.buildClustersFromDb(dataset, next)
 		},
 		function(clusters, next) {
